e1c65f6fc4909492eaf46d15e706a77ba52dc0d7
Teddo <twa2509@gmail.com>
2023-07-14 18:02:28 +0000

    Swagger ui

--- Back/src/main/java/com/devoteam/pmo/configuration/SwaggerConfig.java
+++ Back/src/main/java/com/devoteam/pmo/configuration/SwaggerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -23,4 +24,15 @@ public class SwaggerConfig {
             .paths(PathSelectors.any())
             .build();
     }
+    @SuppressWarnings("unused")
+	private ApiInfo apiInfo() {
+        @SuppressWarnings("deprecation")
+		ApiInfo apiInfo = new ApiInfo(
+                "Spring Boot Swagger Tutorial",
+                "Tutorial for how to use Swagger in spring boot",
+                "1.0",
+                "Terms of service XXX", "okok", "lklk", "lklkl");
+        return apiInfo;
+    }
+
 }
